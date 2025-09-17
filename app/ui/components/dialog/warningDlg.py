78b83e154a31967354effa6e1f45d14c1fb1200d
Cornelius <valyandrianimpanana@gmail.com>
2025-09-18 02:23:40 +0300

    [improve] ui warning dialog

--- app/ui/components/dialog/warningDlg.py
+++ app/ui/components/dialog/warningDlg.py
@@ -17,22 +17,27 @@ from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
     QPalette, QPixmap, QRadialGradient, QTransform)
 from PySide6.QtWidgets import (QAbstractButton, QApplication, QDialog, QDialogButtonBox,
     QLabel, QSizePolicy, QWidget)
+import Icons_rc
 
 class Ui_w_warningDlg(object):
     def setupUi(self, w_warningDlg):
         if not w_warningDlg.objectName():
             w_warningDlg.setObjectName(u"w_warningDlg")
-        w_warningDlg.resize(377, 193)
-        w_warningDlg.setStyleSheet(u"background-color: rgb(237, 51, 59);")
+        w_warningDlg.resize(377, 187)
+        icon = QIcon()
+        icon.addFile(u":/utils/icon/crisis.png", QSize(), QIcon.Mode.Normal, QIcon.State.Off)
+        w_warningDlg.setWindowIcon(icon)
+        w_warningDlg.setStyleSheet(u"background-color: rgb(0, 0, 0);")
         self.buttonBox = QDialogButtonBox(w_warningDlg)
         self.buttonBox.setObjectName(u"buttonBox")
         self.buttonBox.setGeometry(QRect(20, 140, 341, 32))
+        self.buttonBox.setStyleSheet(u"background-color: rgb(147, 147, 145);")
         self.buttonBox.setOrientation(Qt.Orientation.Horizontal)
         self.buttonBox.setStandardButtons(QDialogButtonBox.StandardButton.Ok)
         self.buttonBox.setCenterButtons(True)
         self.label = QLabel(w_warningDlg)
         self.label.setObjectName(u"label")
-        self.label.setGeometry(QRect(0, 40, 401, 81))
+        self.label.setGeometry(QRect(0, 30, 377, 91))
         font = QFont()
         font.setPointSize(14)
         font.setWeight(QFont.DemiBold)
@@ -52,7 +57,7 @@ class Ui_w_warningDlg(object):
     # setupUi
 
     def retranslateUi(self, w_warningDlg):
-        w_warningDlg.setWindowTitle(QCoreApplication.translate("w_warningDlg", u"Dialog", None))
+        w_warningDlg.setWindowTitle(QCoreApplication.translate("w_warningDlg", u"Avertissement", None))
         self.label.setText(QCoreApplication.translate("w_warningDlg", u"Mot de passe non identique.", None))
     # retranslateUi
 
